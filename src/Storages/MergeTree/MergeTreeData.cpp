d56c1a65a666989b967ababd3154c8925a2756f7
Sema Checherinda <checherinda@gmail.com>
2025-07-04 11:02:39 +0200

    fix variadic types

--- src/Storages/MergeTree/MergeTreeData.cpp
+++ src/Storages/MergeTree/MergeTreeData.cpp
@@ -4846,7 +4846,6 @@ DataPartsVector MergeTreeData::grabActivePartsToRemoveForDropRange(
 
 Strings getPartFiles(MergeTreeData::MutableDataPartPtr & part)
 {
-    constexpr std::string proj_suffix = ".proj";
     auto & storage = part->getDataPartStorage();
 
     Strings files_in_part;
@@ -4909,15 +4908,22 @@ void MergeTreeData::checkChecksumsFileIsConsistentWithFileSystem(MutableDataPart
         files_in_checksums.begin(), files_in_checksums.end(),
         std::back_inserter(extra_files));
 
+    Strings files_in_checksums_with_size;
+    for (const auto & file : files_in_checksums)
+    {
+        auto it = part->checksums.files.find(file);
+        chassert(it != part->checksums.files.end());
+        files_in_checksums_with_size.push_back(fmt::format("{} ({})", file, it->second.file_size));
+    }
+
     LOG_DEBUG(getLogger("checkChecksumsFileIsConsistentWithFileSystem"),
-            "checksums.txt file is not consistent with the files on file system, "
             "checksums.txt file has {} files, part '{}' has {} files, "
             "files in checksums: {}, files in part: {}"
             "Missed files in part: {}, Extra files in part: {}",
             part->checksums.files.size(),
             part->name,
             files_in_part.size(),
-            fmt::join(files_in_checksums, ", "),
+            fmt::join(files_in_checksums_with_size, ", "),
             fmt::join(files_in_part, ", "),
             fmt::join(missed_files, ", "),
             fmt::join(extra_files, ", "));
@@ -4931,7 +4937,7 @@ void MergeTreeData::checkChecksumsFileIsConsistentWithFileSystem(MutableDataPart
             part->checksums.files.size(),
             part->name,
             files_in_part.size(),
-            fmt::join(files_in_checksums, ", "),
+            fmt::join(files_in_checksums_with_size, ", "),
             fmt::join(files_in_part, ", "),
             fmt::join(missed_files, ", "),
             fmt::join(extra_files, ", "));
